95f65bee78b66e5fc03b2f6b75fe016402b4ec63
praveenniroula1 <101380394+praveenniroula1@users.noreply.github.com>
2023-12-30 11:10:29 +1030

    fetched product

--- src/Pages/Homepage.js
+++ src/Pages/Homepage.js
@@ -2,11 +2,27 @@ import React, { useEffect } from "react";
 import Layout from "../Layout/Layout";
 import { useAuth } from "../Context/Auth";
 import axios from "axios";
+import { Link } from "react-router-dom";
+import { Checkbox } from "antd";
 
 const Homepage = () => {
   const [auth, setAuth] = useAuth();
   const [products, setProducts] = useAuth([]);
   const [categories, setCategories] = useAuth([]);
+
+  const getAllcategory = async () => {
+    const { data } = await axios.get("/api/v1/category/get-category");
+    if (data.success) {
+      setCategories(data?.category);
+    }
+    try {
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    getAllcategory();
+  }, []);
 
   // get products
   const getAllProducts = async () => {
@@ -26,6 +42,11 @@ const Homepage = () => {
       <div className="row">
         <div className="col-md-3">
           <h4 className="text-center">FIlter by Category</h4>
+          {categories?.map((c) => (
+            <Checkbox key={c._id} onChange={(e) => console.log(e)}>
+              {c.name}
+            </Checkbox>
+          ))}
         </div>
         <div className="col-md-9">
           {" "}
@@ -34,19 +55,31 @@ const Homepage = () => {
             {" "}
             {products?.map((p) => {
               return (
-                <div className="card m-2" style={{ width: "20rem" }}>
-                  <img
-                    src={`/api/v1/product/product-photo/${p._id}`}
-                    className="card-img-top"
-                    alt={p.name}
-                  />
-                  <div className="card-body">
-                    <h5 className="card-title">{p.name}</h5>
-                    <p className="card-text">{p.description}</p>
+                <Link
+                  key={p._id}
+                  to={`/dashboard/admin/product/${p.slug}`}
+                  className="product-link"
+                >
+                  <div className="card m-2" style={{ width: "20rem" }}>
+                    <img
+                      src={`/api/v1/product/product-photo/${p._id}`}
+                      className="card-img-top"
+                      alt={p.name}
+                    />
+                    <div className="card-body">
+                      <h5 className="card-title">{p.name}</h5>
+                      <p className="card-text">{p.description}</p>
+                      <button className="btn btn-primary ms-1">
+                        More Details
+                      </button>
+                      <button className="btn btn-secondary ms-1">
+                        More Details
+                      </button>
+                    </div>
                   </div>
-                </div>
+                </Link>
               );
-            })}
+            })}{" "}
           </div>
         </div>
       </div>
